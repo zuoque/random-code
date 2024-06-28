ac11a6dbf2818a6cd1af3fcc72905b2ed0ff6f7c
zuoque <zuoque1024@163.com>
2024-06-28 11:45:46 +0800

    fix: 修复及优化组织机构生成校验逻辑

--- src/packages/orgCode.ts
+++ src/packages/orgCode.ts
@@ -21,10 +21,23 @@ export function generate(baseCode?:string): string{
     }
 
     if (!isValid) {
-        baseCode = ""; // 先重置成空字符串
+        // 先重置成空字符串
+        baseCode = "";
+        // 是否包含了字母
+        let hasNumber = false;
         // 生成前8位代码并转换成字符串
+        // 为了生成的代码看着不那么随意，这里写了个生成规则
+        // 1. 字母最多占有前3位，3位之后全是数字
+        // 2. 如果字母后是数字，则随后所有位上都必须是数字
+        // 3. 如果是数字开头，则后续所有位上必须是数字
+        // 以上三条规则保证字母和数字不会交叉在一块，并且数字不会出现在字母前面
         while (baseCode.length < BASE_CODE_COUNT) {
-            baseCode += CHARS_STR[randomInt(CHARS_STR.length - 1)];
+            hasNumber = hasNumber || /\d/.test(baseCode);
+            if (hasNumber || baseCode.length >= 3) {
+                baseCode += CHARS_STR[randomInt(9)];
+            } else {
+                baseCode += CHARS_STR[randomInt(CHARS_STR.length - 1)];
+            }
         }
     }
     const c9 = getVerityCode(baseCode as string);
@@ -37,7 +50,7 @@ export function generate(baseCode?:string): string{
  * @returns {string}
  */
 export function getVerityCode(baseCode: string): string {
-    if (!baseCode) return "";
+    if (!baseCode || baseCode.length < 8) return "";
     // 按位加权求和
     let total = WEIGHTS.reduce((sum, wi, i) => {
         sum += (wi * MOD_CODE_MAP.indexOf(baseCode[i]))
@@ -59,5 +72,6 @@ export function validate(code: string): boolean {
     if (!code) return false;
     code = code.trim();
     let verityCode = getVerityCode(code.substring(0, 8));
-    return code.endsWith(verityCode);
+    // 最后一位是校验码，带连字符
+    return code.endsWith("-" + verityCode);
 }
